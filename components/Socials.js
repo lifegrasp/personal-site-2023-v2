94883f219f89305ab65cc48a9af76d90e49c0dac
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-03-13 16:12:36 +0800

    added Socials component

---
+++ components/Socials.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Socials() {
+  return (
+    <div>
+      <Link href="#">GitHub</Link>
+      <Link href="#">LinkedIn</Link>
+      <Link href="#">Mail</Link>
+    </div>
+  );
+}
