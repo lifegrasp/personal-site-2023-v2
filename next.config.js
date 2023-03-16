d4f276201392781ae5789b51309677f06e5bc563
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-03-16 11:27:22 +0800

    added domain in config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ["avatars.githubusercontent.com"],
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "example.com",
+    //     port: "",
+    //     pathname: "/account123/**",
+    //   },
+    // ],
   },
 };
 
