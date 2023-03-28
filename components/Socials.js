8ac11464f8eac31666648b10dafc5118e46928aa
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-03-28 09:54:26 +0800

    refactored

--- components/Socials.js
+++ components/Socials.js
@@ -1,34 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/Socials.module.css"
+import styles from "../styles/Socials.module.css";
 
 export default function Socials() {
+  const Social = (props) => {
+    const { link, imgSrc, alt } = props;
+
+    return (
+      <Link href={link} target="_blank">
+        <Image src={imgSrc} alt={alt} width={100} height={100} />
+      </Link>
+    );
+  };
+
   return (
     <div className={styles.socials}>
-      <Link href="#">
-        <Image
-          src="/../public/svg/github.svg"
-          alt="GitHub icon"
-          width={100}
-          height={100}
-        />
-      </Link>
-      <Link href="#">
-        <Image
-          src="/../public/svg/linkedin.svg"
-          alt="LinkedIn icon"
-          width={100}
-          height={100}
-        />
-      </Link>
-      <Link href="#">
-        <Image
-          src="/../public/svg/mail.svg"
-          alt="Mail icon"
-          width={100}
-          height={100}
-        />
-      </Link>
+      <Social
+        link="https://github.com/lifegrasp"
+        imgSrc="/../public/svg/github.svg"
+        alt="GitHub icon"
+      />
+      <Social
+        link="https://www.linkedin.com/in/henry-hart-alegrado-15953524b/"
+        imgSrc="/../public/svg/linkedin.svg"
+        alt="LinkedIn icon"
+      />
+      <Social
+        link="mailto:henryhartalegrado@gmail.com"
+        imgSrc="/../public/svg/mail.svg"
+        alt="Mail icon"
+      />
     </div>
   );
 }
