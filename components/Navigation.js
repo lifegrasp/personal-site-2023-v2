f86812f2ec37a39bef3207899530a522228dd8be
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-04-07 11:22:51 +0800

    Navigation component sticks to the top now

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navigation.module.css";
 
 export default function Navigation() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 80) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navigation = isScrolled
+    ? `${styles.nav} ${styles.scrolled}`
+    : styles.nav;
+
   return (
-    <nav className={styles.navigation}>
+    <nav className={navigation}>
       <ul>
         <li>
           <Link href="/">Home</Link>
