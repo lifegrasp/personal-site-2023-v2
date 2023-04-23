76d98f73d4af51d7adf33749fdb850bf6fc264f4
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-04-23 09:53:57 +0800

    click sound effect

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  // clicking sound effect
+  useEffect(() => {
+    const playSound = () => {
+      const audio = new Audio("/click.mp3");
+      audio.play();
+    };
+
+    document.addEventListener("click", playSound);
+
+    return () => {
+      document.removeEventListener("click", playSound);
+    };
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
