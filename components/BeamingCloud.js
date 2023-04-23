f6e0cf02c33c278ba7cf091faa30e7ce0fc01fc7
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2023-04-23 13:12:06 +0800

    added BeamingCloud component

---
+++ components/BeamingCloud.js
@@ -0,0 +1,13 @@
+import styles from "../styles/BeamingCloud.module.css";
+
+export default function BeamingCloud() {
+  return (
+    <Image
+      classname={styles.beamingCloud}
+      src="/../public/cloud-w-beaming-smile.png"
+      alt="a cloud with a beaming smile"
+      width={500}
+      height={500}
+    />
+  );
+}
